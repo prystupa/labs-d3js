ff836359c241b9acf9c0000572cb9bc99824c0d9
Eugene Prystupa <prystupa@sqlapi.com>
2012-08-11 10:25:16 -0400

    switch to underscore engine

--- app.js
+++ app.js
@@ -3,23 +3,26 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var engines = require('consolidate');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'html');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+
+  app.engine('html', engines.underscore);
 });
 
 app.configure('development', function(){
