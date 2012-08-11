3c9ac064e9974574ac92cf819c3a13baa744f169
unknown <eprystupa@eprys-mbp-w7.lab49.com>
2012-08-11 18:05:10 -0400

    add floor2 stub

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/flotr2', routes.flotr2);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.flotr2 = function(req, res){
+	res.render('flotr2', { title: 'Flotr2'});
+}
